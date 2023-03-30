4e7017ceeba15638828b3b657c14e9762c7c55f9
CarlinhoBarros <carlosmonteiro92@hotmail.com>
2023-03-30 17:44:03 -0300

    Criando o Projeto para teste de tipos e variáveis

---
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -0,0 +1,6 @@
+public class TiposVariaveis {
+    public static void main(String[] args) throws Exception {
+        String frase = "Hello, World!\nThis is my first print in Java Language!";
+        System.out.println(frase);
+    }
+}
